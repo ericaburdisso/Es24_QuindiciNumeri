2fa23e5ec0c13afaea32f2edfa0114835d37b7d2
ericaburdisso <e.burdisso.3497@vallauri.edu>
2025-05-05 21:01:44 +0200

    consegna

--- index.js
+++ index.js
@@ -1,58 +1,67 @@
 window.addEventListener("load", function () {
+    creaMatrice();
 
-    creamatrice();
-    caricamatrice();
+    caricaMatrice();
+});
 
-})
-
-function creamatrice() {
+function creaMatrice() {
     const wrapper = document.getElementById("wrapper");
 
     for (let i = 0; i < 4; i++) {
         for (let j = 0; j < 4; j++) {
             const div = document.createElement("div");
             div.addEventListener("click", divClick);
-            div.id = `div-${i / 4}-${i % 4}`;
+            div.id = `div-${i}-${j}`;
             wrapper.appendChild(div);
         }
-
     }
 }
+
 function divClick() {
+    const i = parseInt(this.id.split('-')[1]);
+    const j = parseInt(this.id.split('-')[2]);
 
-    const i = this.id.split('-')[1];
-    const j = this.id.split('-')[2];
-
-    //cella sopra
-    if (j - 1 >= 0) {
-        const sopra = document.getElementById(`div-${i}-${j - 1}`);
+    // Cella sopra
+    if (i - 1 >= 0) {
+        const sopra = document.getElementById(`div-${i - 1}-${j}`);
 
         if (sopra.textContent == "") {
             scambiaDiv(this, sopra);
             return;
         }
-
     }
-    //cella destra
+
+    // Cella destra
     if (j + 1 < 4) {
         const destra = document.getElementById(`div-${i}-${j + 1}`);
-        if (sopra.textContent == "") {
+
+        if (destra.textContent == "") {
             scambiaDiv(this, destra);
             return;
         }
     }
 
-    //cella sotto
-    if(i+1>=0)
-    {
-        const sotto=document.getElementById(`div-${i}-${j - 1}`);
+    // Cella sotto
+    if (i + 1 < 4) {
+        const sotto = document.getElementById(`div-${i + 1}-${j}`);
 
-        if(sotto==textContent=="")
-        {
+        if (sotto.textContent == "") {
+            scambiaDiv(this, sotto);
+            return;
+        }
+    }
 
+    // Cella sopra
+    if (j - 1 >= 0) {
+        const sinistra = document.getElementById(`div-${i}-${j - 1}`);
+
+        if (sinistra.textContent == "") {
+            scambiaDiv(this, sinistra);
+            return;
         }
     }
 }
+
 function scambiaDiv(piena, vuota) {
     vuota.textContent = piena.textContent;
     vuota.style.backgroundColor = "blue";
@@ -61,16 +70,18 @@ function scambiaDiv(piena, vuota) {
     piena.style.backgroundColor = null;
 }
 
-function caricamatrice() {
+function caricaMatrice() {
     for (let cont = 1; cont <= 15; cont++) {
+        let divRnd;
+
         do {
             const i = Math.floor(Math.random() * 4);
             const j = Math.floor(Math.random() * 4);
+
             divRnd = document.getElementById(`div-${i}-${j}`);
-        } while (divRnd.textContent != " ");
+        } while (divRnd.textContent != "");
 
         divRnd.style.backgroundColor = "blue";
         divRnd.textContent = cont;
     }
 }
-
